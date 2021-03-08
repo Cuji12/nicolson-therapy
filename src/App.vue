2c642a86ec9ebbd2755f001e40a5598ac026a435
Cu Janeway <cuj.janeway@hotmail.com>
2021-03-08 14:07:29 +0000

    Scaffold project.

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <nav id="nav" class="">
+    <img alt="Nicolson Therapy Logo" src="" />
+    <div class="">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+      <router-link to="/blog">Blog</router-link> |
+      <router-link to="/contact">Conctact</router-link>
+      <router-link to="/FAQs">FAQs</router-link> |
+      <router-link to="/testimonials">Testimonials</router-link>
+    </div>
+  </nav>
+  <router-view/>
+  <footer>
+    <div>
+      <img alt="Nicolson Therapy Logo" src="" />
+      <router-link to="/therapy-contract">Therapy Contract</router-link>
+      <p>© 2021 Nicolson Therapy</p>
+    </div>
+    <div>
+      <p>Get in Touch</p>
+      <a mailto="nicolson@nicolson-therapy.co.uk">nicolson@nicolson-therapy.co.uk</a>
+      <a tel="">555 5555 555</a>
+    </div>
+    <div>
+      <p>Help and Advice</p>
+      <p>FAQ</p>
+      <p>Contact</p>
+    </div>
+    <div>
+      <p>Quick Links</p>
+      <p>Home</p>
+      <p>About</p>
+      <p>Blog</p>
+      <p>Testimonials</p>
+    </div>
+    <p>Nicolson Therapy Ltd a company incorporated and registered in England and Wales with company number LORUM whose registered office is at 7 Rippenden, Manchester, England, M21 9SS</p>
+  </footer>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <section>
+    <img alt="Nicola Nicolson" src="" />
+    <h1>Nicola Nicolson</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim 
+      ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
+      in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
+      mollit anim id est laborum
+    </p>
+  </section>
+  <section>
+    <h3>What is Therapy?</h3>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
+      fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+    </p>
+    <router-link to="/">Lorum</router-link> |
+    <img alt="" src="" />
+  </section>
+    <section>
+    <img alt="" src="" />
+    <h3>Therapy I Provide.</h3>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
+      fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+    </p>
+    <router-link to="/">Lorum</router-link> |
+  </section>
+    <section>
+    <h3>Lorum Ipsum Dulce.</h3>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
+      fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+    </p>
+    <router-link to="/">Lorum</router-link> |
+    <img alt="" src="" />
+  </section>
+</template>
