c4b8f5dbedd2d3eee7874197ce28b2efd2fc8752
Cu Janeway <cuj.janeway@hotmail.com>
2021-04-28 15:15:58 +0100

    Contact quick update.

--- src/main.js
+++ src/main.js
@@ -2,13 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 import router from './router'
-import * as VueGoogleMaps from 'vue2-google-maps'
 
-Vue.use(VueGoogleMaps, {
-    load: {
-      key: 'AIzaSyCCeEEuke3Absp4598jGE8mBz5Z7VZmouA',
-      libraries: 'places',
-    }
-  });
+
 
 createApp(App).use(router).mount('#app')
